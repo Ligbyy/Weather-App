454e958204d0bca15b4a680fc6bb8be6be206c0d
Daniel <danielrojasa15@gmail.com>
2023-07-19 21:03:59 -0400

    updated html,css,and implemented api

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-var x = 5;
-var y = 7;
-console.log(x + y);
+const APIKey = "cb1afb3a2eb8f1a1dcc55e23da0f2634";
+            //const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=orlando&units=imperial";
+            const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=orlando";
+
+
+            async function getWeather(){
+                const response = await fetch(apiURL + `&appid=${APIKey}`);
+                var data = await response.json();
+
+                console.log(data);
+            }
+
+            getWeather();
